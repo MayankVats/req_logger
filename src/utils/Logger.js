3561b64f4bbd1b59c65b6fc2fb9ced307db409b9
mayank011 <mv@rapidinnovation.dev>
2023-06-20 14:40:48 +0530

    format

--- src/utils/Logger.js
+++ src/utils/Logger.js
@@ -2,7 +2,12 @@ import winston, { format } from "winston";
 
 const logConfig = {
   level: process.env.NODE_ENV === "production" ? "info" : "debug",
-  format: format.json(),
+  format: format.combine(
+    format.timestamp({
+      format: "MMM-DD-YYYY HH:mm:ss",
+    }),
+    format.prettyPrint()
+  ),
   transports: [new winston.transports.Console()],
 };
 
